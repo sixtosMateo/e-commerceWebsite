d8f4f93b596c19615d40b9d2576b2fdc63623be7
mateosixtos <sixtosmateo@gmail.com>
2019-01-18 17:25:47 -0800

    adding styled to Product cardcomponent

--- e-commerce/src/components/Product.js
+++ e-commerce/src/components/Product.js
@@ -15,7 +15,7 @@ class Product extends Component {
               <img src={img} alt="product" className="card-img-top"/>
             </Link>
 
-            <button className="cart-btn" disabled={inCart?true:false} onClick={()=>{console.log("addedToCart")}}>
+            <button className="card-btn" disabled={inCart?true:false} onClick={()=>{console.log("addedToCart")}}>
             {
               inCart?(<p className="text-capitalize mb-0" disabled>in cart</p>):
               <i className="fas fa-cart-plus"/>
@@ -35,6 +35,70 @@ class Product extends Component {
   }
 }
 
-const  ProductWrapper = styled.div``
+const ProductWrapper = styled.div`
+.card{
+  border-color: transparent;
+  transition: all 1s linear;
+}
+
+.card-footer{
+  background: transparent;
+  border-top: transparent;
+  transition: all 1s linear;
+}
+
+&:hover{
+  .card{
+    border:0.04rem solid rgba(0,0,0,0.2);
+    box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.02);
+  }
+  .card-footer{
+    background: rgba(247, 247, 247)
+
+  }
+}
+
+.img-container{
+  position:relative;
+  overflow:hidden;
+}
+
+// image growth transition in seconds time or else immediately
+.card-img-top{
+  transition: all 1s linear;
+}
+
+// makes image bigger sizw
+.img-container:hover .card-img-top{
+  transform:scale(1.2);
+
+}
+
+// setting button to parent container when parent container position is relative
+.card-btn{
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 0.2rem 0.4rem;
+  background:var(--lightblue);
+  border: none;
+  color: var(--mainwhite);
+  font-size: 1.4rem;
+  border-radius: 0.5rem 0 0 0;
+  transform: translate(100%,100%);
+  transition: all 1s linear;
+}
+
+
+.img-container:hover .card-btn{
+  transform: translate(0,0);
+}
+
+
+.card-btn:hover{
+  color:var(--mainblue);
+  cursor:pointer;
+}
+`
 
 export default Product;
