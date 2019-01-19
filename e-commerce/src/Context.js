3f988e2bedd4d335d68182435dbd76a56ca6c0da
mateosixtos <sixtosmateo@gmail.com>
2019-01-19 14:39:24 -0800

    adding functionality for addToCart and handle detail

--- e-commerce/src/Context.js
+++ e-commerce/src/Context.js
@@ -10,7 +10,8 @@ class ProductProvider extends Component {
   state={
     products: [],
     // not destructuring because we are not changing the values here
-    detailProduct: detailProduct
+    detailProduct: detailProduct,
+    cart:[]
   }
 
   componentDidMount(){
@@ -30,17 +31,37 @@ class ProductProvider extends Component {
 
     this.setState(()=>{
       return {products}
-    })
+    });
   }
 
+  getItem =(id)=>{
+    const product =this.state.products.find(item=>item.id===id);
+    return product;
+  }
 
-  handleDetail=()=>{
-    console.log("hello from detail")
+  handleDetail=(id)=>{
+    const product = this.getItem(id);
+    this.setState(()=>{
+      return {detailProduct:product}
+    })
 
   }
 
   addToCart =(id)=>{
-    console.log(id)
+    // dont what to use the find method because if change the values in cart Link
+    // we can change it in our product link as well
+    const tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id))
+    const product = tempProducts[index]
+    product.inCart = true;
+    product.count =1;
+    const price = product.price;
+    product.total = price;
+    this.setState(()=>{
+      return { products:tempProducts, cart:[...this.state.cart, product] }
+    }, ()=>{
+      console.log(this.state);
+    });
   }
 
   render() {
--- e-commerce/src/components/Product.js
+++ e-commerce/src/components/Product.js
@@ -12,19 +12,25 @@ class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div className="img-container p-5" onClick={()=>{console.log("click")}}>
-            <Link to="/details">
-              <img src={img} alt="product" className="card-img-top"/>
-            </Link>
+          <ProductConsumer>
+          {(value)=>{
+            return(
+              <div className="img-container p-5" onClick={()=>{value.handleDetail(id)}}>
+                <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top"/>
+                </Link>
 
-            <button className="card-btn" disabled={inCart?true:false} onClick={()=>{console.log("addedToCart")}}>
-            {
-              inCart?(<p className="text-capitalize mb-0" disabled>in cart</p>):
-              <i className="fas fa-cart-plus"/>
-            }
-            </button>
+                <button className="card-btn" disabled={inCart?true:false} onClick={()=>{value.addToCart(id)}}>
+                {
+                  inCart?(<p className="text-capitalize mb-0" disabled>in cart</p>):
+                  <i className="fas fa-cart-plus"/>
+                }
+                </button>
 
-          </div>
+              </div>
+            )
+          }}
+          </ProductConsumer>
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
             <h5 className="text-blue font-italic mb-0">
